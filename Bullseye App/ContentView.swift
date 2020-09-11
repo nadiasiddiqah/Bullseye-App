f8e672d59610a9aa4a299ee6139b174832fffbb2
nadiasiddiqah <nadiasiddiqah@gmail.com>
2020-09-11 14:13:20 -0700

    Initial Commit

---
+++ Bullseye App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye App
+//
+//  Created by Nadia Siddiqah on 9/11/20.
+//  Copyright © 2020 Nadia Siddiqah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
